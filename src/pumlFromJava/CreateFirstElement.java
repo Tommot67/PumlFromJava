320b387d4e0ddca7ed4b27a875f199817e06ce4b
yglady <yann.glady67@gmail.com>
2023-05-15 09:51:54 +0200

    Commentaires 2

--- src/pumlFromJava/CreateFirstElement.java
+++ src/pumlFromJava/CreateFirstElement.java
@@ -8,22 +8,31 @@ public class CreateFirstElement extends ElementDefault {
     public CreateFirstElement(Element element) {
         super(element);
     }
+
+    // run() définit le type de l'élément et crée une instance appropriée.
     public void run(){
         for (Element element1 : super.getElementEnclosed()){
             switch (element1.getKind()) {
                 case CLASS:
+                    //Crée une instance de classe
+                    //builduml = l'entiereté de l'élément
                     CreateClassElement newClassElement = new CreateClassElement(element1);
                     this.buildUml += newClassElement.getStringElementAll();
                     break;
                 case INTERFACE:
+                    //Crée une instance d'interface
+                    //builduml = juste les lignes de début et de fin de l'élément
                     CreateInterElement newInterElement = new CreateInterElement(element1);
                     this.buildUml += newInterElement.getStringElementStartEnd();
                     break;
                 case ENUM:
+                    //Crée une instance d'enum
+                    //builduml = l'entiereté de l'élément
                     CreateEnumElement newEnumElement = new CreateEnumElement(element1);
                     this.buildUml += newEnumElement.getStringElementAll();
                     break;
                 case PACKAGE:
+                    //Crée un nouvel élément puis le fait passer par la fonction chaque élément du package
                     CreateFirstElement newFirstElement = new CreateFirstElement(element1);
                     newFirstElement.run();
                     break;
